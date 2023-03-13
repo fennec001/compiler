249d6ced1ec40620dada0996cc88efbe864eefcf
fennec001 <109749936+fennec001@users.noreply.github.com>
2023-03-12 23:32:35 -0300

    projeto inicial

---
+++ src/main/java/Compilador/Interface.java
@@ -0,0 +1,49 @@
+package Compilador;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class Interface {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interface window = new Interface();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Interface() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 584, 409);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBorder(new NumberedBorder());
+		textArea.setBounds(196, 36, 342, 309);
+		frame.getContentPane().add(textArea);
+	}
+}
